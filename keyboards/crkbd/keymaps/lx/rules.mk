7cded4ba417b390f3dd5484527b6dd95a44b155d
lxhan <lxhan@protonmail.com>
2022-02-03 19:23:06 +0900

    add crkbd custom layout

---
+++ keyboards/crkbd/keymaps/lx/rules.mk
@@ -0,0 +1,16 @@
+
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+LTO_ENABLE = yes
+WPM_ENABLE = yes
+BACKLIGHT_ENABLE = no
+LED_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
